35177747ceb931685a7ad94420cec6fec4cf233a
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2013-09-13 15:32:30 -0400

    Add new page called partners

---
+++ partners.php
@@ -0,0 +1,24 @@
+<?php
+	require_once 'header.html';
+?>
+<body>
+<div id="container" class="ccolor">
+	<div id="header">
+		<img class="prepend-2" src="images/olympic-logo-FINAL.jpg" alt="olympic logo image" style="float:left"/>
+    <div class="push-3 addtext alt">Servicing all your<br />Chartering Needs<br>(704)450-3289</div>
+	
+	</div>
+	<div id="sidebar" class="vert">
+   <ul>
+      <li><a href="index.html">Home</a></li>
+   </ul>
+ 	</div>
+	<div id="content">
+        <p class="ctext">partners</p>
+        <a href="http://www.visitcabarrus.com/">
+        <img src="http://www.visitcabarrus.com/images/cabarrus_county_cvb.png" alt="Cabarrus County Convention & Visitors Bureau"/></a>
+        <p></p>
+    </div>
+<?php 
+	require_once 'footer.html';
+?>
