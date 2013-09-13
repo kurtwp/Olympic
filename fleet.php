442a76a425fcc7690360615f4c2d213fc9dc6ac2
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2013-09-13 15:58:21 -0400

    Add new page called fleet

---
+++ fleet.php
@@ -0,0 +1,23 @@
+<?php
+	require_once 'header.html';
+?>
+<body>
+<div id="container" class="ccolor">
+	<div id="header">
+		<img class="prepend-2" src="images/olympic-logo-FINAL.jpg" alt="olympic logo image" style="float:left"/>
+    <div class="push-3 addtext alt">Servicing all your<br />Chartering Needs<br>(704)450-3289</div>
+	
+	</div>
+	<div id="sidebar" class="vert">
+   <ul>
+      <li><a href="index.html">Home</a></li>
+   </ul>
+ 	</div>
+	<div id="content">
+        <p class="ctext">fleet</p>
+        <p class="ptext"> 2 - 14 passenger Eldorado buses </p>
+        <p class="ptext"> 2 - 12 passenger Turtle Top Van Terra buses </p>
+    </div>
+<?php
+	require_once 'footer.html';
+?>
