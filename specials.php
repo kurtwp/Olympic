83203fce2913c50e034b1cfe24a08bd5c9059909
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2013-09-13 15:10:04 -0400

    Removed bad text from file

--- specials.php
+++ specials.php
@@ -16,7 +16,7 @@
 	<div id="content">
         <p class="ctext">September special</p>
         <p class="subtext prepend-1">This special offer is only extended to new clients and new student riders to Olympic:</p>
-        <p class="ptext">In celebration of completing our 2nd year in operation Olympic is offering one way morning or one way afternoon transportation to either Pine Lake Preparatory or Mountain Island Charter school for the entire month of September for <strong>$45.00</strong>.  That’s 8+26+11!  Price is per student and sibling discounts do not apply.  Check out the web site <a href="www.rideolympic.com">www.rideolympic.com</a> for pick up and drop off locations and times.  The offer expires on <strong>Monday, September 9, 2013</strong>.  Seats are limited.  Call 704-450-3289 to reserve your spot! </p><br />
+        <p class="ptext">In celebration of completing our 2nd year in operation Olympic is offering one way morning or one way afternoon transportation to either Pine Lake Preparatory or Mountain Island Charter school for the entire month of September for <strong>$45.00</strong>.  That’s 8+26+11!  Price is per student and sibling discounts do not apply.  Check out the web site <a href="http://www.rideolympic.com">www.rideolympic.com</a> for pick up and drop off locations and times.  The offer expires on <strong>Monday, September 9, 2013</strong>.  Seats are limited.  Call 704-450-3289 to reserve your spot! </p><br />
     </div>
 <?php
 	require_once 'footer.html';
