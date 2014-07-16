4551c73fbb222cd682394827319a6906f8c41608
kurtwp <kurt@pasewaldt.com>
2014-07-16 13:42:58 -0400

    removed specials

--- specials.php
+++ specials.php
@@ -14,13 +14,11 @@
    </ul>
  	</div>
 	<div id="content">
-        <p class="ctext">October special</p>
-        <p class="subtext prepend-1">This special offer is only extended to new clients and new student riders to Olympic:</p>
-        <p class="ptext">One Way Morning Transportation from the Skybrook/Highland Creek Area to the Pine Lake Prep Campus  for the entire month of October  is only <strong>$45.00</strong>.  Sibling discounts do not apply to this offer. Discount is based on central location pick up at either one of our two locations in the Skybrook/Highland Creek area.  Please check out the web site at <a href="http://www.rideolympic.com">www.rideolympic.com</a>  to see pick up times and locations.  This offer expires on <strong>October 11, 2013</strong>.  Call 704-450-3289 to reserve your spot!</p>
-        <br />
-        <p class="ctext">September special</p>
-        <p class="subtext prepend-1">This special offer is only extended to new clients and new student riders to Olympic:</p>
-        <p class="ptext">In celebration of completing our 2nd year in operation Olympic is offering one way morning or one way afternoon transportation to either Pine Lake Preparatory or Mountain Island Charter school for the entire month of September for <strong>$45.00</strong>.  That’s 8+26+11!  Price is per student and sibling discounts do not apply.  Check out the web site <a href="http://www.rideolympic.com">www.rideolympic.com</a> for pick up and drop off locations and times.  The offer expires on <strong>Monday, September 9, 2013</strong>.  Seats are limited.  Call 704-450-3289 to reserve your spot! </p><br />
+        <p class="ctext">specials</p>
+        <p class="subtext prepend-1"> - </p>
+        <p class="ptext">
+		<br />
+        
     </div>
 <?php
 	require_once 'footer.html';
