f95ac4f01cdf7512b3a7d33178274834f1fb62af
kurtwp <kurt@pasewaldt.com>
2014-07-18 16:28:31 -0400

    Changed locations

--- school_ccs.php
+++ school_ccs.php
@@ -42,34 +42,19 @@
                 <td> 3:25 </td>
             </tr>
             <tr class="stext">
-                <td> Hampton Inn, 19501 Statesville Road, Cornelius </td>
+                <td> Hampton Inn, (19501 Statesville Road, Cornelius) </td>
                 <td> 7:25 </td>
                 <td> 3:15 </td>
             </tr>
             <tr class="stext">
-                <td> Brookway Drive, Huntersville <br />(parking lot area by Journey Church and Asthma Center) </td>
+                <td> Brookway Drive, Huntersville </td>
                 <td> 7:35 </td>
                 <td> - </td>
             </tr>
 	    <tr class="stext">
-                <td> Brookway Drive, Huntersville <br />(parking lot area by Journey Church and Asthma Center) </td>
-                <td> 7:35 </td>
-                <td> - </td>
-            </tr>
-	    <tr class="stext">
-                <td> Earth Fare, Huntersville <br />(this may be moved more towards the Goddard School but in the general direction of Earth Fare) </td>
+                <td> Earth Fare, Huntersville </td>
                 <td> 7:45 </td>
                 <td> 3:00 </td>
-            </tr>
-	    <tr class="stext">
-                <td> University City United Methodist Church (all 5th graders will unload) </td>
-                <td> 8:00 </td>
-                <td> 2:15 </td>
-            </tr>
-	    <tr class="stext">
-                <td> Corvian Campus </td>
-                <td> 8:15 </td>
-                <td> 2:30 </td>
             </tr>
 	    <tr class="stext">
                 <td> 9620 Northcross Center Court, Huntersville </td>
