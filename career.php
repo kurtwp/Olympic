10af4560218c61017b238274050dfc2a0b62e9cc
kurtwp <kurt@pasewaldt.com>
2014-12-16 14:38:27 -0500

    New page for careers

---
+++ career.php
@@ -0,0 +1,40 @@
+<?php
+	require_once 'header.html';
+?>
+<body>
+<div id="container" class="ccolor">
+	<div id="header">
+		<img class="prepend-2" src="images/olympic-logo-FINAL.jpg" alt="olympic logo image" style="float:left"/>
+    <div class="push-3 addtext alt">Servicing all your<br />Chartering Needs<br>(704)450-3289</div>
+	
+	</div>
+	<div id="sidebar" class="vert">
+   <ul>
+      <li><a href="index.html">Home</a></li>
+      <!-- <li><a href="#">Services</a>
+        <ul>
+            <li><a href="cschool.php">Charter Schools</a></li>
+			<li><a href="oncall.php">On Call</a></li>
+			<li><a href="pcharter.php">Private Charter</a></li>
+		<!--	<li><a href="sumcamp.php">Summer Camp</a></li> -->
+      <!--  </ul>
+      </li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a class="active" href="#">Careers</a></li> -->
+   </ul>
+ 	</div>
+	<div id="content">
+		<p class="ctext">careers</p>
+    <p class="ptext">Right mouse click on any of the forms below and select either "Save Target As" or "Save Link As"
+	to save the form to your hard drive.</p>
+		<br />
+		<p class="ctext ppara">Application</p>
+		
+		<a class="ptext" href="downloads/application.pdf">Application</a><br />
+		<img src="images/white162px.png" alt="white spacer" />
+	</div>
+	
+<?php
+	require_once 'footer.html';
+?>
